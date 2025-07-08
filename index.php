3b7221c5c294fe5df93802f6d0213682682f20a1
Marcus <macke_125@hotmail.com>
2025-07-09 00:13:59 +0200

    start

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// THmanagement - Index Page
+// Created: July 9, 2025
+
+// Start session
+session_start();
+
+// Include configuration file (if you create one later)
+// include_once 'config/config.php';
+
+// Include database connection (if you create one later)
+// include_once 'config/database.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THmanagement - Home</title>
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUa6c4WG0TJAqo8BK4ZO4lxU87lO9wbHg3LzCq/MzS8QZqYoqO8FWVn5J8J" crossorigin="anonymous">
+</head>
+<body>
+    
+
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
